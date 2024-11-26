800124dcae7e672e770b6efe905d58258ed04be8
carla <carla.fueber@educa.jcyl.es>
2024-11-26 10:52:42 +0100

    Commit

--- src/main/java/com/example/proyecto_hibernate/controllers/CrearProfesorController.java
+++ src/main/java/com/example/proyecto_hibernate/controllers/CrearProfesorController.java
@@ -1,17 +1,20 @@
 package com.example.proyecto_hibernate.controllers;
 
-import com.example.proyecto_hibernate.util.ComprobacionesYcambioEscena;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 
-public class InicioSesionController {
+public class CrearProfesorController {
 
     @FXML
-    private Button bt_iniciar_sesion;
+    private Button bt_crear;
+
+    @FXML
+    private ComboBox<?> cb_tipo;
 
     @FXML
     private ImageView imagen_fondo;
@@ -20,14 +23,14 @@ public class InicioSesionController {
     private PasswordField pwd_contrasena;
 
     @FXML
+    private TextField txt_nombre;
+
+    @FXML
     private TextField txt_numero;
 
     @FXML
-    void onIniciarSesionClick(ActionEvent event) {
-        //comprobar que no este vacio
-        //comprobar que exista
-        //comprobar si es profesor o jefe de estudios
-        ComprobacionesYcambioEscena.abrirEscena("inicio-jefe-estudios.fxml", "Jefe de estudios");
+    void onCrearClick(ActionEvent event) {
+
     }
 
 }
