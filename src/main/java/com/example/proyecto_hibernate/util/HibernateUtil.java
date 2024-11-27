5e7d530551f0dcd5b5fe5c502a5740bfc982e012
carla <carla.fueber@educa.jcyl.es>
2024-11-27 09:34:29 +0100

    Commit

--- src/main/java/com/example/proyecto_hibernate/util/HibernateUtil.java
+++ src/main/java/com/example/proyecto_hibernate/util/HibernateUtil.java
@@ -1,8 +1,6 @@
 package com.example.proyecto_hibernate.util;
 
-import com.example.proyecto_hibernate.classes.Alumnos;
-import com.example.proyecto_hibernate.classes.Grupos;
-import com.example.proyecto_hibernate.classes.Profesor;
+import com.example.proyecto_hibernate.classes.*;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -17,6 +15,8 @@ public class HibernateUtil {
         cfg.addAnnotatedClass(Alumnos.class);
         cfg.addAnnotatedClass(Grupos.class);
         cfg.addAnnotatedClass(Profesor.class);
+        cfg.addAnnotatedClass(ParteIncidencia.class);
+        cfg.addAnnotatedClass(PuntuacionPartes.class);
 
         System.out.println("conectado hibernate");
 
