fd451fc0962a58fbb33f723819293f6234f8cd17
caarlafb <carladelafuente7@gmail.com>
2024-12-10 13:20:42 +0100

    Commit

--- src/main/java/com/example/proyecto_hibernate/classes/ColorParte.java
+++ src/main/java/com/example/proyecto_hibernate/classes/ColorParte.java
@@ -1,13 +1,16 @@
 package com.example.proyecto_hibernate.classes;
 
+//Clase que representa el color de los partes, asociando a cada uno su código de color y los puntos.
 public enum ColorParte {
     VERDE("#a0ffa0", 1),
     NARANJA("#ffce86", 6),
     ROJO("#ff6e6e", 12);
 
+    //ATRIBUTOS
     private final String codigo_color;
     private final int puntos;
 
+    //CONSTRUCTOR
     ColorParte(String codigo_color, int puntos) {
         this.codigo_color = codigo_color;
         this.puntos = puntos;
--- src/main/java/com/example/proyecto_hibernate/classes/TipoProfesor.java
+++ src/main/java/com/example/proyecto_hibernate/classes/TipoProfesor.java
@@ -1,5 +1,6 @@
 package com.example.proyecto_hibernate.classes;
 
+//Clase que representa los tipos de profesores que existen
 public enum TipoProfesor {
     jefe_de_estudios,
     profesor
